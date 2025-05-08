ddb75e026b6690cf666f4f3e2103c04aaac27147
SytexMC <sytexmc@duck.com>
2025-05-08 13:29:01 +0200

    🐛 Fix inconsistent knockback when switching armor before end crystal explosion by handling knockback in ExplosionPrimeEvent

--- src/main/java/me/sytex/knockback/listeners/EntityExplodeListener.java
+++ src/main/java/me/sytex/knockback/listeners/EntityExplodeListener.java
@@ -4,14 +4,14 @@ import me.sytex.knockback.handlers.KnockbackHandler;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 
 public class EntityExplodeListener implements Listener {
 
   @EventHandler(ignoreCancelled = true)
-  public void onEntityExplode(EntityExplodeEvent event) {
+  public void onEntityExplode(ExplosionPrimeEvent event) {
     if (event.getEntity().getType() != EntityType.END_CRYSTAL) return;
 
-    KnockbackHandler.handle(event.getLocation());
+    KnockbackHandler.handle(event.getEntity().getLocation());
   }
 }
